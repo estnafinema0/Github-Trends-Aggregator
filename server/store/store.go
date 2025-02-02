19b17edad3371dcdab6e7c22eab16295342fb1b0
estnafinema0 <Kat9765830@gmail.com>
2025-02-02 22:34:38 +0300

    Implement GetRepos method in store

--- server/store/store.go
+++ server/store/store.go
@@ -1,14 +1,24 @@
 package store
 
-import "github.com/estnafinema0/Github-Trends-Aggregator/models"
+import "github.com/estnafinema0/Github-Trends-Aggregator/server/models"
 
 type Store struct {
+	repos map[string]models.Repository
 }
 
 func NewStore() *Store {
 	return &Store{}
 }
 
+// GetRepos returns repositories by language
 func (s *Store) GetRepos(language string) []models.Repository {
-	return []models.Repository{}
+	result := []models.Repository{}
+
+	for _, repo := range s.repos {
+		if repo.Language == language {
+			result = append(result, repo)
+		}
+	}
+
+	return result
 }
