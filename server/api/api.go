e1a685fbdc13ef376951a6e0d4edface41281af8
estnafinema0 <Kat9765830@gmail.com>
2025-02-02 22:23:00 +0300

    Add initial API handler for trends endpoint

---
+++ server/api/api.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"net/http"
+	// "github.com/estnafinema0/Github-Trends-Aggregator/server"
+)
+
+// GetTrendsHandler returns handler for GET /trends
+func GetTrendsHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
--- server/go.mod
+++ server/go.mod
@@ -2,6 +2,4 @@ module github.com/estnafinema0/github-trend-aggregator/server
 
 go 1.23
 
-require (
-	github.com/gorilla/mux v1.8.0
-)
+require github.com/gorilla/mux v1.8.0
