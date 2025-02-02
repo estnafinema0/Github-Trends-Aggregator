651a5bb5da6987208b555664706ea8bb888a660f
estnafinema0 <Kat9765830@gmail.com>
2025-02-02 22:45:41 +0300

    Implement GetTrendsHandler

--- server/api/api.go
+++ server/api/api.go
@@ -1,16 +1,19 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
-	// "github.com/estnafinema0/Github-Trends-Aggregator/server"
+
+	"github.com/estnafinema0/Github-Trends-Aggregator/server/store"
 )
 
 // GetTrendsHandler returns handler for GET /trends
 func GetTrendsHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		//language := r.URL.Query().Get("language")
+	return func(rw http.ResponseWriter, r *http.Request) {
+		language := r.URL.Query().Get("language")
+		repos := store.GetRepos(language)
+		rw.Header().Set("Content-Type", "application/json")
 
-		w.Header().Set("Content-Type", "application/json")
-
+		json.NewEncoder(rw).Encode(repos)
 	}
 }
